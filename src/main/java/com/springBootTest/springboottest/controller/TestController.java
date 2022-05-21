59624e3ddaa2ac98ce85c4d186506353b22ea547
lucyfurt <lucianafurtado10@gmail.com>
2022-05-21 02:29:02 -0300

    test integracao

--- src/main/java/com/springBootTest/springboottest/controller/TestController.java
+++ src/main/java/com/springBootTest/springboottest/controller/TestController.java
@@ -1,10 +1,11 @@
 package com.springBootTest.springboottest.controller;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    public String saudacao(String nome){
+    public String saudacao(@RequestParam(name="nome", defaultValue="DIO")String nome){
         return String.format("Bem-vindo, %s", nome);
     }
 }
